a5baaedcdf2531dad89463e70d9d0a5e73003c03
TZY <1137721268@qq.com>
2020-12-29 18:29:16 +0800

    啊这

---
+++ Spider1.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+def get_html(url='http://www.pythonscraping.com/'):
+    try:
+        html=urlopen(url=url)
+    except HTTPError as e:
+        return '网络错误'+e
+    try:
+        bs=BeautifulSoup(html,'lxml')
+        title=bs.body.h1#暂时捕捉body中的h1，其他的部分可以放在事后优化
+    except AttributeError as e:
+        return '捕捉错误'+e
+    return title
+
+print(get_html())
