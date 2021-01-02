05e35d4329528a7d2f8522192572512b4d070cd0
TZY <1137721268@qq.com>
2021-01-02 16:48:34 +0800

    scrapy

---
+++ tutorial/tutorial/items.py
@@ -0,0 +1,12 @@
+# Define here the models for your scraped items
+# 项目项定义文件
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TutorialItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
